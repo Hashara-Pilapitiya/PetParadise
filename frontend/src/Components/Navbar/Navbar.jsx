14f2ea9aedd3e55a5c3321d8d3d5aae0b3297e26
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-14 19:23:55 +0530

    Enable dropdown menu in Navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -27,8 +27,27 @@ const Navbar = () => {
                     <li onClick={() => {setMenu('birds')}}><Link to='/birds'>Birds{menu === 'birds' ? <hr /> : <></>}</Link></li>
                     <li onClick={() => {setMenu('mammals')}}><Link to='/mammals'>Small Mammals{menu === 'mammals' ? <hr /> : <></>}</Link></li>
                     <li onClick={() => {setMenu('reptilia')}}><Link to='/reptilia'>Reptilia{menu === 'reptilia' ? <hr /> : <></>}</Link></li>
+
+                </ul> 
+
+                {/* <ul>
+                <li onClick={() => {setMenu('shop')}}><Link to='/'>Shop{menu === 'shop' ? <hr /> : <></>}</Link></li>
+                <li onClick={() => {setMenu('categories')}}><Link to='/categories'>Categories{menu === 'categories' ? <hr /> : <></>}</Link>
+                <div className='dropdown-menu'>
+                    <ul>
+                    <li onClick={() => {setMenu('dogs')}}><Link to='/dogs'>Dogs{menu === 'dogs' ? <hr /> : <></>}</Link></li>
+                    <li onClick={() => {setMenu('cats')}}><Link to='/cats'>Cats{menu === 'cats' ? <hr /> : <></>}</Link></li>
+                    <li onClick={() => {setMenu('fish')}}><Link to='/fish'>Fish{menu === 'fish' ? <hr /> : <></>}</Link></li>
+                    <li onClick={() => {setMenu('birds')}}><Link to='/birds'>Birds{menu === 'birds' ? <hr /> : <></>}</Link></li>
+                    <li onClick={() => {setMenu('mammals')}}><Link to='/mammals'>Small Mammals{menu === 'mammals' ? <hr /> : <></>}</Link></li>
+                    <li onClick={() => {setMenu('reptilia')}}><Link to='/reptilia'>Reptilia{menu === 'reptilia' ? <hr /> : <></>}</Link></li>
+                    </ul>
+                </div>
+                </li>
+                <li onClick={() => {setMenu('about')}}><Link to='/about'>About{menu === 'about' ? <hr /> : <></>}</Link></li>
+                <li onClick={() => {setMenu('contact')}}><Link to='/contact'>Contact{menu === 'contact' ? <hr /> : <></>}</Link></li>
+                </ul> */}
                    
-                </ul>
             </div>
 
             <div className='login-cart'>
