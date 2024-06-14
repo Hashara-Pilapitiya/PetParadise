c5aeaf713c68de89eacc6fa2d4d88277eaf7ecf4
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-14 10:55:46 +0530

    Add Hero component to Shop page

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Hero.css';
+import HeroImage from '../Assets/hero.png';
+
+const Hero = () => {
+  return (
+    <>
+
+        <div className='main'>
+
+        <div className='main_left'>
+            <div className='main_left_title'>
+                <h1>Welcome to Your Pet's Perfect Paradise!</h1>
+            </div>
+            <div className='main_left_content'>
+                <p>Discover a wide range of pets, supplies, and expert care tips at PetParadise. We're dedicated to helping you keep your pets happy and healthy with top-quality products and friendly service.</p>
+            </div>
+        </div>
+
+        <div className='main_right'>
+            <img src={HeroImage} alt='Hero' />
+        </div>
+
+        </div>
+
+    </>
+  )
+}
+
+export default Hero;
--- frontend/src/Pages/Shop/Shop.jsx
+++ frontend/src/Pages/Shop/Shop.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Hero from '../../Components/Hero/Hero';
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+
+      <Hero />
+
+    </div>
   )
 }
 
