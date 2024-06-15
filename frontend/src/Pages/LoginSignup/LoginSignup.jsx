0d1b5b3619be48175aa1aeee11645502cc569b68
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-15 13:26:29 +0530

    Update background gradient colors and improve form styling

--- frontend/src/Pages/LoginSignup/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup/LoginSignup.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import './LoginSignup.css';
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+
+      <div className='loginsignup-container'>
+        <h1>Sign Up</h1>
+        <div className='loginsignup-fields'>
+          <input type='text' placeholder='Your Name...' />
+          <input type='email' placeholder='Email Address...' />
+          <input type='password' placeholder='Password...' />
+        </div>
+
+        <button>Continue</button>
+
+        <p className="loginsignup-login">
+          Already have an account? <a href='/login'><span>Login Here</span></a>
+        </p>
+
+        <div className="loginsignup-agree">
+          <input type='checkbox' />
+          <p>By signing up, I agree to your Terms & Conditions.</p>
+        </div>
+
+        </div>
+      </div>
+
+      
   )
 }
 
