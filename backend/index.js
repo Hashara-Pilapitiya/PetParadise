69c6a573398955f19090922bad7107f7e5e4ae89
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-16 16:36:23 +0530

    Optimize responsive design for AnimalDisplay, Navbar, CartItems, LoginSignup, ShopCategory, Footer, NewsLetter, AnimalCard, Banner, Popular, Hero components

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,32 @@
-import React from 'react'
+const port = 4000;
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
+const express = require('express');
+const app = express();
 
-export default index
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+const e = require('express');
+
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect ('mongodb+srv://hasharanethmi2020:oLrnq67RZ0lfCGLz@cluster0.5fqolpu.mongodb.net/petparadise');
+
+// API creation
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+
+
+app.listen(port, (err) => {
+  if (!err) {
+    console.log(`Server is running on port: ` + port);
+  } else {
+    console.log(err);
+  }
+});
