3c6fec3a7d321399bbfe828000b508eee838e68e
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-17 07:31:20 +0530

    Add Admin Panel

---
+++ admin/src/assets/all_animals.js
@@ -0,0 +1,376 @@
+import Dog1 from '../Assets/dog1.jpg';
+import Dog2 from '../Assets/dog2.jpg';
+import Dog3 from '../Assets/dog3.jpg';
+import Dog4 from '../Assets/dog4.jpg';
+import Dog5 from '../Assets/dog5.jpeg';
+import Dog6 from '../Assets/dog6.jpg';
+import Dog7 from '../Assets/dog7.avif';
+import Dog8 from '../Assets/dog8.jpg';
+import Dog9 from '../Assets/dog9.jpg';
+import Dog11 from '../Assets/dog11.avif';
+import Dog12 from '../Assets/dog12.jpg';
+import Dog13 from '../Assets/dog13.jpg';
+import Dog14 from '../Assets/dog14.jpg';
+import Dog15 from '../Assets/dog15.jpg';
+import Dog16 from '../Assets/dog16.jpeg';
+import Cat1 from '../Assets/cat1.jpg';
+import Cat2 from '../Assets/cat2.jpg';
+import Cat3 from '../Assets/cat3.jpg';
+import Cat4 from '../Assets/cat4.jpg';
+import Cat5 from '../Assets/cat5.jpg';
+import Cat6 from '../Assets/cat6.jpg';
+import Cat7 from '../Assets/cat7.jpeg';
+import Cat8 from '../Assets/cat8.jpg';
+import Cat9 from '../Assets/cat9.jpeg';
+import Cat10 from '../Assets/cat10.jpeg';
+import Fish1 from '../Assets/fish1.jpeg';
+import Fish2 from '../Assets/fish2.jpg';
+import Fish3 from '../Assets/fish3.jpg';
+import Fish4 from '../Assets/fish4.jpg';
+import Bird1 from '../Assets/bird1.jpg';
+import Bird2 from '../Assets/bird2.jpg';
+import Bird3 from '../Assets/bird3.jpg';
+import Bird4 from '../Assets/bird4.jpg';
+import Bird5 from '../Assets/bird5.jpg';
+import Mammal1 from '../Assets/mammal1.jpg';
+import Mammal2 from '../Assets/mammal2.jpg';
+import Mammal3 from '../Assets/mammal3.jpg';
+import Mammal4 from '../Assets/mammal4.jpg';
+import Mammal5 from '../Assets/mammal5.png';
+import Reptilia1 from '../Assets/reptilia1.jpg';
+import Reptilia2 from '../Assets/reptilia2.jpg';
+
+let all_animals = [
+    {
+      id: 1,
+      name: "Rottweiler",
+      category: "Dogs",
+      image: Dog1,
+      price: 50.0,
+      age: 2,
+    },
+    {
+      id: 2,
+      name: "Boxer",
+      category: "Dogs",
+      image: Dog2,
+      price: 85.0,
+      age: 3,
+    },
+    {
+      id: 3,
+      name: "German Shepherd",
+      category: "Dogs",
+      image: Dog3,
+      price: 60.0,
+      age: 4,
+    },
+    {
+      id: 4,
+      name: "Australian Shepherd",
+      category: "Dogs",
+      image: Dog4,
+      price: 100.0,
+      age: 1,
+    },
+    {
+      id: 5,
+      name: "Labrador Retriever",
+      category: "Dogs",
+      image: Dog5,
+      price: 85.0,
+      age: 2,
+    },
+    {
+      id: 6,
+      name: "Golden Retriever",
+      category: "Dogs",
+      image: Dog6,
+      price: 85.0,
+      age: 3,
+    },
+    {
+      id: 7,
+      name: "Cocker Spaniel",
+      category: "Dogs",
+      image: Dog7,
+      price: 85.0,
+      age: 4,
+    },
+    {
+      id: 8,
+      name: "Pointer",
+      category: "Dogs",
+      image: Dog8,
+      price: 85.0,
+      age: 1,
+    },
+    {
+      id: 9,
+      name: "Collie",
+      category: "Dogs",
+      image: Dog9,
+      price: 85.0,
+      age: 2,
+    },
+    {
+      id: 11,
+      name: "Pomeranian",
+      category: "Dogs",
+      image: Dog11,
+      price: 85.0,
+      age: 3,
+    },
+    {
+        id: 12,
+        name: "Yorkshire Terrier",
+        category: "Dogs",
+        image: Dog12,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 13,
+        name: "Pug",
+        category: "Dogs",
+        image: Dog13,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 14,
+        name: "Jack Russell Terrier",
+        category: "Dogs",
+        image: Dog14,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 15,
+        name: "Labradoodle",
+        category: "Dogs",
+        image: Dog15,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 16,
+        name: "Akita",
+        category: "Dogs",
+        image: Dog16,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 17,
+        name: "British Shorthair",
+        category: "Cats",
+        image: Cat1,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 18,
+        name: "American Shorthair",
+        category: "Cats",
+        image: Cat2,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 19,
+        name: "Manx",
+        category: "Cats",
+        image: Cat3,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 20,
+        name: "Devon Rex",
+        category: "Cats",
+        image: Cat4,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 21,
+        name: "Persian",
+        category: "Cats",
+        image: Cat5,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 22,
+        name: "Maine Coon",
+        category: "Cats",
+        image: Cat6,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 23,
+        name: "Balinese",
+        category: "Cats",
+        image: Cat7,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 24,
+        name: "Siberian",
+        category: "Cats",
+        image: Cat8,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 25,
+        name: "Burmese",
+        category: "Cats",
+        image: Cat9,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 26,
+        name: "Savannah",
+        category: "Cats",
+        image: Cat10,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 27,
+        name: "Goldfish",
+        category: "Fish",
+        image: Fish1,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 28,
+        name: "Bettas",
+        category: "Fish",
+        image: Fish2,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 29,
+        name: "Guppies",
+        category: "Fish",
+        image: Fish3,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 30,
+        name: "Angelfish",
+        category: "Fish",
+        image: Fish4,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 31,
+        name: "Parakeets",
+        category: "Birds",
+        image: Bird1,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 32,
+        name: "Canaries",
+        category: "Birds",
+        image: Bird2,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 33,
+        name: "Finches",
+        category: "Birds",
+        image: Bird3,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 34,
+        name: "Parrots",
+        category: "Birds",
+        image: Bird4,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 35,
+        name: "Cockatiels",
+        category: "Birds",
+        image: Bird5,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 36,
+        name: "Hamsters",
+        category: "Small Mammals",
+        image: Mammal1,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 37,
+        name: "Guinea Pigs",
+        category: "Small Mammals",
+        image: Mammal2,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 38,
+        name: "Rabbits",
+        category: "Small Mammals",
+        image: Mammal3,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 39,
+        name: "Ferrets",
+        category: "Small Mammals",
+        image: Mammal4,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 40,
+        name: "Gerbils",
+        category: "Small Mammals",
+        image: Mammal5,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 41,
+        name: "Turtles",
+        category: "Reptiles",
+        image: Reptilia1,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 42,
+        name: "Tortoises",
+        category: "Reptiles",
+        image: Reptilia2,
+        price: 85.0,
+        age: 3,
+      },
+    
+
+    ];
+
+    export default all_animals;
---
+++ admin/src/assets/data.js
@@ -0,0 +1,53 @@
+import Dog3 from './dog3.jpg';
+import Cat5 from './cat5.jpg';
+import Fish1 from './fish1.jpeg';
+import Bird1 from './bird1.jpg';
+import Mammal2 from './mammal2.jpg';
+import Reptilia1 from './reptilia1.jpg';
+
+let data_animal = [
+    {
+        id: 3,
+        name: "German Shepherd",
+        image: Dog3,
+        price: 60.0,
+        age: 4,
+      },
+      {
+        id: 21,
+        name: "Persian",
+        image: Cat5,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 27,
+        name: "Goldfish",
+        image: Fish1,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 31,
+        name: "Parakeets",
+        image: Bird1,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 34,
+        name: "Guinea Pigs",
+        image: Mammal2,
+        price: 85.0,
+        age: 3,
+      },
+      {
+        id: 39,
+        name: "Turtles",
+        image: Reptilia1,
+        price: 85.0,
+        age: 3,
+      },
+];
+
+export default data_animal;
